9d71e6a0e460261fbb90bb610032a1ed6457a9e3
Kaarle Järvinen <kaarle46@gmail.com>
2023-10-17 12:32:52 +0300

    Added body for task 1 and 2

--- main.c
+++ main.c
@@ -7,17 +7,31 @@ int main() {
    pid_t pid = getpid();
    printf("Process ID: %d\n", pid);
 
+   // Task 1: Get input
+   char input[100];
+   printf("Enter the test string: ");
+   fgets(input, sizeof(input), stdin);
+
+   // Here we create a pipe for communication between P0 and C0
+   int pipe_fd[2];
+   if (pipe(pipe_fd) == -1) {
+      printf("Pipe creation failed");
+      exit(1);
+   }
+
+   // Here we fork a child process
    pid_t child_pid = fork();
 
+   // Here we execute code conditionally based on the process.
    if (child_pid < 0) {
-      printf("Fork failed");
-      sleep(1);
+      // Here forking fails and we exit the program
+      printf("Fork failed \n");
+      exit(1);
    } else if (child_pid == 0) {
-      printf("task2");
-      sleep(1);
+      printf("Task 2 here\n");
    } else {
-      printf("parent process");
-      sleep(1);
+      // Here we read data from child process and set it to the input variable.
+      printf("Task 1 here\n");
    }
    
    return 0;
